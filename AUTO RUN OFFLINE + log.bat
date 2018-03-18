33d1390f7b24311306d8d7111989defabd8e37c4
Jerry1211 <simonblaney1@gmail.com>
2018-03-18 11:35:30 +1100

    Fixed batch

--- AUTO RUN OFFLINE + log.bat
+++ AUTO RUN OFFLINE + log.bat
@@ -1,5 +1,7 @@
 @echo off
 for /f "tokens=14" %%a in ('ipconfig ^| findstr IPv4') do set _IPaddr=%%a
 echo YOUR IP ADDRESS IS: %_IPaddr%
-java -jar target\pubg-radar-1.0-SNAPSHOT-jar-with-dependencies.jar %_IPaddr% PortFilter %_IPaddr% Offline > logall.txt
+java -jar target\RadarProject-Jerry1211-FORK-jar-with-dependencies.jar %_IPaddr% PortFilter %_IPaddr% Offline > logall.txt
+
+
 pause
--- AUTO RUN ONLINE.bat
+++ AUTO RUN ONLINE.bat
@@ -4,5 +4,5 @@ echo YOUR IP ADDRESS IS: %_IPaddr%
 echo "RUNNING PUBG-RADAR"
 set /p game=ENTER GAME PC IP:
 echo "%game%"
-java -jar target\pubg-radar-1.0-SNAPSHOT-jar-with-dependencies.jar %_IPaddr% PortFilter %game%
+java -jar target\RadarProject-Jerry1211-FORK-jar-with-dependencies.jar %_IPaddr% PortFilter %game%
 pause
