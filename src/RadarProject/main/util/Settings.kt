6561fced1c430ec7e2b6e0d56434e8be89e23ee2
Unknown <scarjit@aol.com>
2018-03-19 01:07:58 +0100

    Added default settings for all fonts

--- src/RadarProject/main/util/Settings.kt
+++ src/RadarProject/main/util/Settings.kt
@@ -8,14 +8,22 @@ import java.io.File
 class Settings {
 
     data class jsonsettings(
+            //AGENCYFB
             val hubFont_size: Int = 30,
             val hubFont_color: Color = Color.WHITE,
+            val hubFontShadow_color: Color = Color(1f,1f,1f,0.4f),
 
             val espFont_size: Int = 16,
             val espFont_color: Color = Color.WHITE,
+            val espFontShadow_color: Color = Color(1f,1f,1f,0.2f),
 
+            //NUMBER
             val largeFont_size: Int = 38,
             val largeFont_color: Color = Color.WHITE,
+
+            //GOTHICB
+            val largeFont_size2: Int = 38,
+            val largeFont_color2: Color = Color.WHITE,
 
             val littleFont_size: Int = 15,
             val littleFont_color: Color = Color.WHITE,
@@ -24,7 +32,36 @@ class Settings {
             val nameFont_color: Color = Color.WHITE,
 
             val itemFont_size: Int = 6,
-            val itemFont_color: Color = Color.WHITE
+            val itemFont_color: Color = Color.WHITE,
+
+            val compaseFont_size: Int = 10,
+            val compaseFont_color: Color = Color(0f, 0.95f, 1f, 1f),
+
+            val compaseFontShadow_color: Color = Color(0f, 0f, 0f, 0.5f),
+
+            val littleFont_size2: Int = 15,
+            val littleFont_color2: Color = Color.WHITE,
+
+            val littleFontShadow_color: Color = Color(0f, 0f, 0f, 0.5f),
+
+            val menuFont_size: Int = 12,
+            val menuFont_color: Color = Color.WHITE,
+
+            val menuFontOn_size: Int = 12,
+            val menuFontOn_color: Color = Color.GREEN,
+
+            val menuFontOFF_size: Int = 12,
+            val menuFontOFF_color: Color = Color.RED,
+
+            val hporange_size: Int = 10,
+            val hporange_color: Color = Color.ORANGE,
+
+            val hpgreen_size: Int = 10,
+            val hpgreen_color: Color = Color.GREEN,
+
+            val hpred_size: Int = 10,
+            val hpred_color: Color = Color.RED
+
             )
 
     val settingsname = "settings.json"
@@ -49,8 +86,7 @@ class Settings {
 
     fun savesettings(settings: jsonsettings) {
         val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
-        val adapter = moshi.adapter(jsonsettings::class.java)
-        val json = adapter.toJson(settings)
+        val json = moshi.adapter(jsonsettings::class.java).indent("  ").toJson(settings)
         val f = File(settingsname)
         f.writeText(json)
     }
